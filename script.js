01d17ed49b6d572d1ed835d8f0a44ae261231296
taurgus <100350635+taurgus@users.noreply.github.com>
2023-05-21 09:10:24 +0300

    animointia

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 $(document).ready(function() { //Dokumenttia ei voi manipuloida, jos se ei ole ready. jQuery tarkistaa tilan tällä funktiolla
+  $('#weather-form').fadeIn(1500); //Animaatio sivun ladattua
   $('#saanappi').click(function() { //Klikkaamalla säänappia haetaan tiedot
-    var city = $('#location').val();
+    var city = $('#location').val(); //Muuttuja kaupungille/sijainnille
     
     // API pyyntö openweatheriin ja pyydetyillä spekseillä
     $.ajax({
-      url: 'https://api.openweathermap.org/data/2.5/weather',
+      url: 'https://api.openweathermap.org/data/2.5/weather', //Linkki mistä tiedot haetaan
       data: {
-        q: city,
+        q: city, //Kaupungin nimi, postinumero tai vastaava
         appid: '5c69cea1adcc7b793afeaf500214c0c5', //API key
         units: 'metric', //Tuulennopeus ja lämpötilat eurooppalaisittain
         lang: 'fi' //Kielenä suomi
@@ -23,9 +24,9 @@ $(document).ready(function() { //Dokumenttia ei voi manipuloida, jos se ei ole r
         weatherInfo += '<div class="weather-info-item">Taivas: ' + response.weather[0].description + '<br>';
         weatherInfo += '<img src="http://openweathermap.org/img/w/' + weatherIcon + '.png">';
 
-        
+        $('#weather-info').html(weatherInfo);    
         //Animaatiot diville ja tekstille
-        $('#weather-info').html(weatherInfo);        
+            
         $('#weather-info').hide().slideDown(1000);
         $('.weather-info-item').hide().fadeIn(1000, 'swing');
       },
