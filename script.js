53818f8b432d99b4141df7500c353d696066aa5a
taurgus <100350635+taurgus@users.noreply.github.com>
2023-05-21 08:12:55 +0300

    kommentteja

--- script.js
+++ script.js
@@ -2,17 +2,17 @@ $(document).ready(function() {
   $('#saanappi').click(function() {
     var city = $('#location').val();
     
-    // API pyyntö openweatheriin
+    // API pyyntö openweatheriin ja pyydetyt tiedot
     $.ajax({
       url: 'https://api.openweathermap.org/data/2.5/weather',
       data: {
         q: city,
-        appid: '5c69cea1adcc7b793afeaf500214c0c5', 
-        units: 'metric', 
-        lang: 'fi'
+        appid: '5c69cea1adcc7b793afeaf500214c0c5', //API key
+        units: 'metric', //Tuulennopeus ja lämpötilat ei amerikkalaisittain
+        lang: 'fi' //Kielenä suomi
       },
       //
-      success: function(response) {
+      success: function(response) { //AJAX success functiolla saadaan säätiedot. 
         var weatherIcon = response.weather[0].icon;
         var weatherInfo = '<div class="weather-info-item">Sää paikassa: ' + city + '<br>';
         weatherInfo += '<div class="weather-info-item">Maa: ' + response.sys.country + '<br>';
@@ -24,11 +24,11 @@ $(document).ready(function() {
         weatherInfo += '<img src="http://openweathermap.org/img/w/' + weatherIcon + '.png">';
 
         
-        
+        //Animaatiot diville ja tekstille
         $('#weather-info').html(weatherInfo);
-                $('#weather-info').hide().slideDown(500);
+                $('#weather-info').hide().slideDown(1000);
 
-        $('.weather-info-item').hide().fadeIn(800, 'swing');
+        $('.weather-info-item').hide().fadeIn(1000, 'swing');
       },
       //Error functiolla ilmoitus, jos kaupunkia ei löydy tms.
       error: function() {
